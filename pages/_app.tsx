d56fdefd9122538e082016d389d80ebce8144999
hemilrajpura-simform <hemil.r@simformsolutions.com>
2022-05-03 11:50:06 +0530

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "reset-css";
+
+
+
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
